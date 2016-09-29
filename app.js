6675e55d4d302ceb7e2eb3e0aa4ae0d2b72009e8
Celso Henrique Taliatelli <celsotaliatelli@hotmail.com>
2016-09-28 23:04:21 -0300

    Add função para alterar classes de status das contas

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ var app = new Vue({
 			{date_due:'21/08/2016',name:"Conta de água",value:35.90,done:1},
 			{date_due:'22/08/2016',name:"Conta de telefone",value:75.90,done:0},
 			{date_due:'23/08/2016',name:"Fatura Cartão de crédito",value:505.90,done:0},
-		]
+		],
+		
 	},
 	computed:{
 		status:function(){
@@ -35,11 +36,31 @@ var app = new Vue({
 
 				if(!this.bills[i].done){
 					count++;
+					
 				}
+			
 			}
 			return !count?"Nenhuma conta a pagar":"Existem " + count + " a serem pagas";
+		},
+
+		statusClass:function(){
+			var count = 0;
+			for(var i in this.bills){
+
+				if(!this.bills[i].done){
+					count++;
+				}
+			}
+			if(count > 0){
+				return 'nao-pago';
+			}else if(count == 0){
+				return 'pago';
+			}else{
+				return 'nao-cadastrado';
+			}
 		}
 	},
+
 	methods:{
 		showView:function(id){
 			this.activedView == id;
@@ -62,22 +83,28 @@ var app = new Vue({
 	delbill:function(bill){
 		if(confirm('Deseja excluir esta conta ?')){
 			this.bill = bill;
-			console.log(this.bill.name);
 			this.bills.splice(this.bill,1);
 		}
 	},
 	loadbill:function(bill){
 		this.bill = bill;
 		this.activedView = 1;
+	},
+	pgBill:function(bill){
+		this.bill = bill;
+		this.bill.done = 1;
+	},
+	nPgBill:function(bill){
+		this.bill = bill;
+		this.bill.done = 0;
 	}
 
 	},
 	
 	
 });
-
 Vue.filter('doneLabel',function(value){
-	if(value == 0){
+	if(value == false){
 		return "Não Paga";
 	}else{
 		return "Paga";
